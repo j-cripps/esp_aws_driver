079418742d77bf14fb731ffd047b12bf9be1eadc
Jack Cripps <jackc@monitoraudio.com>
2019-10-18 16:39:42 +0100

    Working on structure of project

--- components/ma_jobs_agent/ma_jobAgent.c
+++ components/ma_jobs_agent/ma_jobAgent.c
@@ -8,6 +8,7 @@
  * @date 11/09/2019
  */
 
+
 #include "ma_jobAgent.h"
 
 #include <stdio.h>
@@ -36,32 +37,45 @@
 #include "esp_flash_partitions.h"
 #include "esp_partition.h"
 
+#include "aws_iot_config.h"
+#include "aws_iot_log.h"
+#include "aws_iot_version.h"
+#include "aws_iot_mqtt_client_interface.h"
+#include "aws_iot_jobs_interface.h"
+
+
 /* -------------------------------------------------------------------------- */
+
 
 #define EXAMPLE_WIFI_SSID "Monitor Audio WiFi"
 #define EXAMPLE_WIFI_PASS "des1gnf0rs0und"
 
+
 /* -------------------------------------------------------------------------- */
+
 
 #define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 
+
 /* -------------------------------------------------------------------------- */
+
 
 static const char *TAG = "ma_JobAgent";
 
 static const char *THING_NAME = "ESP32TestThing";
 
 /* FreeRTOS event group to signal when we are connected & ready to make a request */
-static EventGroupHandle_t job_agent_event_group;
+static EventGroupHandle_t wifi_event_group;
 
 /* The event group allows multiple bits for each event,
-   but we only care about one event - are we connected
-   to the AP with an IP? */
+ * but we only care about one event - are we connected
+ * to the AP with an IP?
+ */
 const int WIFI_CONNECTED_BIT = BIT0;
 
 /* CA Root certificate, device ("Thing") certificate and device
  * ("Thing") key.
-*/
+ */
 extern const uint8_t aws_root_ca_pem_start[] asm("_binary_aws_root_ca_pem_start");
 extern const uint8_t aws_root_ca_pem_end[] asm("_binary_aws_root_ca_pem_end");
 extern const uint8_t certificate_pem_crt_start[] asm("_binary_certificate_pem_crt_start");
@@ -69,23 +83,41 @@ extern const uint8_t certificate_pem_crt_end[] asm("_binary_certificate_pem_crt_
 extern const uint8_t private_pem_key_start[] asm("_binary_private_pem_key_start");
 extern const uint8_t private_pem_key_end[] asm("_binary_private_pem_key_end");
 
+/**
+ * @brief Default MQTT HOST URL is pulled from the aws_iot_config.h
+ */
+char HostAddress[255] = AWS_IOT_MQTT_HOST;
+
+/**
+ * @brief Default MQTT port is pulled from the aws_iot_config.h
+ */
+uint32_t port = AWS_IOT_MQTT_PORT;
+
+
 /* Private Function Prototypes
  * -------------------------------------------------------------------------- */
 
+
 static void __attribute__((noreturn)) task_fatal_error(void);
 static esp_err_t event_handler(void *ctx, system_event_t *event);
+static bool wifiProvisionedCheck(void);
+static void initialiseWifi(void);
 static bool diagnostic(void);
-static void initialise_wifi(void);
 static void bootValidityCheck(void);
-static void nvsBootCheck(void);
+static esp_err_t nvsBootCheck(void);
+static void otaBootCheck(void);
+void disconnectCallbackHandler(AWS_IoT_Client *pClient, void *data);
+static void connectToAWS(void);
+
 
 /* -------------------------------------------------------------------------- */
+
 
 static void __attribute__((noreturn)) task_fatal_error(void)
 {
     ESP_LOGE(TAG, "Exiting task due to fatal error...");
     (void)vTaskDelete(NULL);
-    while(1) {};
+    // while(1) {};
 }
 
 
@@ -96,13 +128,13 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
         esp_wifi_connect();
         break;
     case SYSTEM_EVENT_STA_GOT_IP:
-        xEventGroupSetBits(job_agent_event_group, WIFI_CONNECTED_BIT);
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
         break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
         /* This is a workaround as ESP32 WiFi libs don't currently
            auto-reassociate. */
         esp_wifi_connect();
-        xEventGroupClearBits(job_agent_event_group, WIFI_CONNECTED_BIT);
+        xEventGroupClearBits(wifi_event_group, WIFI_CONNECTED_BIT);
         break;
     default:
         break;
@@ -111,7 +143,17 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
 }
 
 
-static void initialise_wifi(void)
+static bool wifiProvisionedCheck(void)
+{
+	bool ret = true;
+
+
+
+	return ret;
+}
+
+
+static void initialiseWifi(void)
 {
     tcpip_adapter_init();
     wifi_event_group = xEventGroupCreate();
@@ -177,7 +219,7 @@ static void bootValidityCheck(void)
 }
 
 
-static void nvsBootCheck(void)
+static esp_err_t nvsBootCheck(void)
 {
 	// Initialise NVS.
 	esp_err_t err = nvs_flash_init();
@@ -188,19 +230,184 @@ static void nvsBootCheck(void)
 		ESP_ERROR_CHECK(nvs_flash_erase());
 		err = nvs_flash_init();
 	}
-	ESP_ERROR_CHECK( err );
+	return err;
+}
+
+
+static void otaBootCheck(void)
+{
+	const esp_partition_t *configured = esp_ota_get_boot_partition();
+	const esp_partition_t *running = esp_ota_get_running_partition();
+
+	if (configured != running)
+	{
+		ESP_LOGW(TAG, "Configured OTA boot partition at offset 0x%08x, but running from offset 0x%08x",
+				 configured->address, running->address);
+		ESP_LOGW(TAG, "This can happen if either the OTA boot data or preferred boot image becomes corrupted.");
+	}
+
+	ESP_LOGI(TAG, "Running partition type %d subtype %d (offset 0x%08x)",
+			 running->type, running->subtype, running->address);
+}
+
+
+void disconnectCallbackHandler(AWS_IoT_Client *pClient, void *data)
+{
+    ESP_LOGW(TAG, "MQTT Disconnect");
+    IoT_Error_t rc = FAILURE;
+
+    if(NULL == pClient) {
+        return;
+    }
+
+    if(aws_iot_is_autoreconnect_enabled(pClient)) {
+        ESP_LOGI(TAG, "Auto Reconnect is enabled, Reconnecting attempt will start now");
+    } else {
+        ESP_LOGW(TAG, "Auto Reconnect not enabled. Starting manual reconnect...");
+        rc = aws_iot_mqtt_attempt_reconnect(pClient);
+        if(NETWORK_RECONNECTED == rc) {
+            ESP_LOGW(TAG, "Manual Reconnect Successful");
+        } else {
+            ESP_LOGW(TAG, "Manual Reconnect Failed - %d", rc);
+        }
+    }
+}
+
+
+static void connectToAWS(void)
+{
+	char cPayload[100];
+	const char *TEST_TOPIC = "test_topic/esp32";
+	const uint8_t TEST_TOPIC_LEN = strlen(TEST_TOPIC);
+
+	IoT_Error_t rc = FAILURE;
+
+    AWS_IoT_Client client;
+    IoT_Client_Init_Params mqttInitParams = iotClientInitParamsDefault;
+    IoT_Client_Connect_Params connectParams = iotClientConnectParamsDefault;
+
+    IoT_Publish_Message_Params paramsQOS0;
+
+    ESP_LOGI(TAG, "AWS IoT SDK Version %d.%d.%d-%s", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_TAG);
+
+    mqttInitParams.enableAutoReconnect = false; // We enable this later below
+    mqttInitParams.pHostURL = HostAddress;
+    mqttInitParams.port = port;
+
+    mqttInitParams.pRootCALocation = (const char *)aws_root_ca_pem_start;
+    mqttInitParams.pDeviceCertLocation = (const char *)certificate_pem_crt_start;
+    mqttInitParams.pDevicePrivateKeyLocation = (const char *)private_pem_key_start;
+
+    mqttInitParams.mqttCommandTimeout_ms = 20000;
+    mqttInitParams.tlsHandshakeTimeout_ms = 5000;
+    mqttInitParams.isSSLHostnameVerify = true;
+    mqttInitParams.disconnectHandler = disconnectCallbackHandler;
+    mqttInitParams.disconnectHandlerData = NULL;
+
+    rc = aws_iot_mqtt_init(&client, &mqttInitParams);
+    if(SUCCESS != rc)
+    {
+        ESP_LOGE(TAG, "aws_iot_mqtt_init returned error : %d ", rc);
+        abort();
+    }
+
+    connectParams.keepAliveIntervalInSec = 10;
+    connectParams.isCleanSession = true;
+    connectParams.MQTTVersion = MQTT_3_1_1;
+    /* Client ID is set in the menuconfig of the example */
+    connectParams.pClientID = CONFIG_AWS_EXAMPLE_CLIENT_ID;
+    connectParams.clientIDLen = (uint16_t) strlen(CONFIG_AWS_EXAMPLE_CLIENT_ID);
+    connectParams.isWillMsgPresent = false;
+
+    ESP_LOGI(TAG, "Connecting to AWS...");
+	do
+	{
+		rc = aws_iot_mqtt_connect(&client, &connectParams);
+		if(SUCCESS != rc)
+		{
+			ESP_LOGE(TAG, "Error(%d) connecting to %s:%d", rc, mqttInitParams.pHostURL, mqttInitParams.port);
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+		}
+	} while(SUCCESS != rc);
+
+	/*
+	 * Enable Auto Reconnect functionality. Minimum and Maximum time of Exponential backoff are set in aws_iot_config.h
+	 *  #AWS_IOT_MQTT_MIN_RECONNECT_WAIT_INTERVAL
+	 *  #AWS_IOT_MQTT_MAX_RECONNECT_WAIT_INTERVAL
+	 */
+	rc = aws_iot_mqtt_autoreconnect_set_status(&client, true);
+	if(SUCCESS != rc)
+	{
+		ESP_LOGE(TAG, "Unable to set Auto Reconnect to true - %d", rc);
+		abort();
+	}
+
+    paramsQOS0.qos = QOS0;
+    paramsQOS0.payload = (void *) cPayload;
+    paramsQOS0.isRetained = 0;
+
+    while((NETWORK_ATTEMPTING_RECONNECT == rc || NETWORK_RECONNECTED == rc || SUCCESS == rc))
+    {
+    	ESP_LOGI(TAG, "In AWS processing loop");
+
+        // Yield pauses the current thread, allowing MQTT send/receive to occur
+        rc = aws_iot_mqtt_yield(&client, 100);
+
+        if(NETWORK_ATTEMPTING_RECONNECT == rc)
+        {
+            // If the client is attempting to reconnect then skip the rest of the loop.
+            continue;
+        }
+
+        sprintf(cPayload, "Hello");
+        paramsQOS0.payloadLen = strlen(cPayload);
+        rc = aws_iot_mqtt_publish(&client, TEST_TOPIC, TEST_TOPIC_LEN, &paramsQOS0);
+
+        ESP_LOGI(TAG, "Stack remaining for task '%s' is %d bytes", pcTaskGetTaskName(NULL), uxTaskGetStackHighWaterMark(NULL));
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
 }
 
 
 void job_agent_task(void *param)
 {
 	bootValidityCheck();
-	nvsBootCheck();
-	initialise_wifi();
+	ESP_ERROR_CHECK(nvsBootCheck());
+	otaBootCheck();
+
+	initialiseWifi();
 
 	while (1)
 	{
-		ESP_LOGI(TAG, "In Task");
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		/* Check if WiFi provisioned */
+		if (wifiProvisionedCheck() == 1)
+		{
+			ESP_LOGI(TAG, "WiFi provisioned");
+		}
+		else
+		{
+			ESP_LOGW(TAG, "WiFi not provisioned, waiting on credentials");
+			vTaskDelay(2000 / portTICK_PERIOD_MS);
+			continue;
+		}
+
+		/* Check if WiFi connected */
+		EventBits_t ret_bit = xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_BIT, false, true, 50 / portTICK_PERIOD_MS);
+		if ((ret_bit & WIFI_CONNECTED_BIT) != 0)
+		{
+			ESP_LOGI(TAG, "WiFi connected");
+		}
+		else
+		{
+			ESP_LOGW(TAG, "WiFi not connected");
+			vTaskDelay(2000 / portTICK_PERIOD_MS);
+			continue;
+		}
+
+		/* Connect to AWS */
+		connectToAWS();
+
+		ESP_LOGE(TAG, "An error occurred in the main loop.");
+		abort();
 	}
 }
